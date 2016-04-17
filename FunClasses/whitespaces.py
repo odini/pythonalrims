b36c4c570d2494cc9d4ddf822c83af440b045b52
eodesem <odeh.semreen@rwth-aachen.de>
2016-04-17 23:33:11 +0200

    Cleanup

--- FunClasses/whitespaces.py
+++
@@ -1,8 +0,0 @@
-def whitespaces(text):
-    result = []
-    for i in xrange(len(text)):
-        if text[i] != ' ':
-            result.append(text[i])
-    return ''.join(result)
-
-print whitespaces('Python is great')
