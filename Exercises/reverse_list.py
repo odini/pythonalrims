fc4d2d2ecc9f4407631a32bd0581394eba5446a1
eodesem <odeh.semreen@rwth-aachen.de>
2016-04-24 19:54:29 +0200

    changed username

--- Exercises/reverse_list.py
+++ Exercises/reverse_list.py
@@ -3,4 +3,10 @@ def reverse(test):
         return test
     return reverse(test[1:]) + [test[0]]
 
-print reverse([1,2,3])
+def rec_reverse(test):
+    result = []
+    for i in xrange(len(test),0,-1):
+        result.append(test[i-1])
+    return result
+
+print rec_reverse([1,2,3])
--- Exercises/reverse_string.py
+++ Exercises/reverse_string.py
@@ -3,4 +3,10 @@ def reverse(text):
         return text
     return reverse(text[1:]) + text[0]
 
-print reverse('odin')
+def rec_reverse(text):
+    result = []
+    for i in xrange(len(text),0,-1):
+        result.append(text[i-1])
+    return ''.join(result)
+
+print rec_reverse('odin')
